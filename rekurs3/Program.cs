2b57ad2c0cf522bc95d28b6cd92a4357e589cdbf
shelihaeva <smelkovagolodnova@yandex.ru>
2023-11-18 10:57:31 +0300

    сложение чисел в рекурсии

---
+++ rekurs3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int SumDigitsInNumber(int number)
+{
+    if(number == 0) return 0;
+    return number % 10 + SumDigitsInNumber(number/10);
+}
+
+int N = ReadInt("Введите число N:");
+Console.WriteLine(SumDigitsInNumber(N));
