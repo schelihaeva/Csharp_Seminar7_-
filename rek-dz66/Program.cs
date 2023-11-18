df1078eb38a7033efd95b0e8ca3c3e253bf87c16
shelihaeva <smelkovagolodnova@yandex.ru>
2023-11-18 13:19:39 +0300

    решение 66 задачи дз

---
+++ rek-dz66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int m = ReadInt("Введите M: ");
+int n = ReadInt("Введите N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSumm(m, n, temp = 0);
+
+void PrintSumm(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {summ} ");
+        return;
+    }
+    PrintSumm(m, n - 1, summ);
+}
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
