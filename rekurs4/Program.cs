0251e89dff25798e0a538f850d268789befe130a
shelihaeva <smelkovagolodnova@yandex.ru>
2023-11-18 11:16:38 +0300

    возведение в степень

--- rekurs4/Program.cs
+++ rekurs4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+ int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int Pow(int number, int power)
+{
+    if (power <= 1) return number;
+    return number*Pow(number,power-1);
+}
+
+
+int A = ReadInt("Введите число А:");
+int B = ReadInt("Введите число B (степень):");
+Console.WriteLine(Pow(A,B));
